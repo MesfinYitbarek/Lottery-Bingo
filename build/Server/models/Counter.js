de5a2f68c708e7fffee7f40374593dbd3eb51a3e
the-special-1 <168620446+the-special-1@users.noreply.github.com>
2024-07-25 18:23:07 +0300

    edit deploy

--- build/Server/models/Counter.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const CounterSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  value: { type: Number, required: true },
-});
-
-const Counter = mongoose.model('Counter', CounterSchema);
-export default Counter
